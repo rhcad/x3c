330fc5e692eae26baba691a837ceac531e5e1a5c
thinkhy <smth.hy@gmail.com>
2011-02-22 06:57:18 +0000

    Translated comments into English.

--- tools/MakePluginPerl/makePlugin_v0.3.pl
+++ tools/MakePluginPerl/makePlugin_v0.3.pl
@@ -1,36 +1,38 @@
 #!/usr/bin/perl
 ####################################################################################
-# Description: X3框架插件创建脚本
+# Description: a Perl script for auto-generation of X3C Plugin 
 # Usage:          
 # Creator:     thinkhy
 # Date:        2010.10.13 
 #
-# ChangeList:  1. 更改接口文件，替换GUID, 完成脚本的基本功能。  [thinkhy 10/10/19]
-#              2. 增加了对配置文件(config.h)的支持。            [thinkhy 10/10/21]
-#              3. 更改资源文件版本信息                          [thinkhy 10/11/15]
+# ChangeList:  1. Implemented the fuction: Modify interface file,replace GUID. Finished basic function of the script. [thinkhy 10/10/19]
+#              2. Added configuration file(config.h). [thinkhy 10/10/21]
+#              3. Implemented the function to change the version information. [thinkhy 10/11/15]
+#              4. Translated comments to English. [thinkhy 10/11/15]
+
 #
-#  TODO: 1. Remove all the SVN information  in the directory of the generated plugin。 [thinkhy 11/01/06]
+#  TODO:       1. Remove all the SVN information in the directory of the generated plugin。 [thinkhy 11/01/06]
 ####################################################################################
 use strict;
 use warnings;
 
-# 用于拷贝目录: dircopy
+# Copy directory: dircopy
 # CPAN  http://search.cpan.org/~dmuey/File-Copy-Recursive-0.38/Recursive.pm
 use File::Copy::Recursive qw(fcopy rcopy dircopy fmove rmove dirmove);
 
-# 用于拷贝文件
+# Copy file
 use File::Copy;
 
-# 用于定位目录
+# Locate work directory
 use Cwd;
 
-# 用于生成GUID: Data::GUID->new
+# Generate GUID: Data::GUID->new
 # http://search.cpan.org/~rjbs/Data-GUID-0.045/lib/Data/GUID.pm
 use Data::GUID;
 
 
 ######################################################
-# 读入配置文件(makePlugin.xml)
+# Read configuration file (makePlugin.xml)
 # http://search.cpan.org/~wadg/Config-IniFiles-2.38/IniFiles.pm
 use Config::IniFiles;
 
@@ -58,41 +60,42 @@ chomp($InternalName);
 chomp($OriginalFilename);
 chomp($ProductName);
 
-# Step1: 拷贝插件目录
+# Step1: Copy directory from template 
 ######################################################
 my $orig = $cfg->val('exampleplugin','plugindir');
 my $interfaceDir = $cfg->val('exampleplugin','interfacedir');
 chomp($orig);
 
-dircopy($orig,$pluginName) or die "拷贝目录从$orig到$pluginName失败";
+dircopy($orig,$pluginName) or die "Failed to copy files from $orig to $pluginName";
 
-# Step2: 更改文件名与文件内容
+# Step2: Change filename and contents
 ######################################################
 my $curdir = getcwd;
-chdir($pluginName); # 定位到插件目录
-# 处理Module
-foreach my $file (glob "*")  { # 查找当前目录下所有文件
+chdir($pluginName); # Locate work directory
+
+# Process moduel directory 
+foreach my $file (glob "*")  { #  Find all the files of current directory 
     print "$file\n";
 
-    # 获取扩展名 
+    # get extenstion name 
     my $extName = ($file =~ m/([^.]+)$/)[0];
 
-    # 处理源代码与工程文件
+    # process source code and project files 
     if ($extName =~ /vcproj|dsp|c|cxx|cpp|h|hpp|rc/i) { 
         open(FILE, $file)
-            or warn "打开文件$file失败!\n";
+            or warn "Failed to open $file!\n";
 
         my @lines = <FILE>;
         close FILE;
 
         open(OUT, "> $file")
-            or warn "打开文件$file失败!\n";
+            or warn "Failed to open $file!\n";
 
         foreach my $line (@lines) {
-            # 替换插件名与工程名
+            # replace plugin name and project name
             $line =~ s/$tmpPrjName/$prjName/g;
             $line =~ s/$tmpPluginName/$pluginName/g;
-            print OUT $line; # 写回到原文件
+            print OUT $line; # Written back to the original file
         }
 
 
@@ -100,34 +103,35 @@ foreach my $file (glob "*")  { # 查找当前目录下所有文件
     }
 
     my $newFile = $file;
-    if ($file =~ /$tmpPrjName/g) # 替换工程名
+    if ($file =~ /$tmpPrjName/g) # replace project name
     {
         $newFile =~ s/$tmpPrjName/$prjName/g; 
         rename $file, $newFile 
     }
     else {
-        if ($file =~ /$tmpPluginName/g) # 替换插件名
+        if ($file =~ /$tmpPluginName/g) # replace plugin name
         {
             $newFile =~ s/$tmpPluginName/$pluginName/g; 
             rename $file, $newFile 
         }
     }
-
 }
+
 ######################################################
 
 
-# STEP3 处理Interface
+# STEP3 Process Interface
 ######################################################
 my $data = Data::GUID->new;
 my $guid = $data->as_string;
 
-# 先定位到原始目录
+# locate the original directory
 chdir($curdir);
 
-# 再定位到接口目录
+# Reloacte to the interface directory  
 chdir($interfaceDir)
-    or die "定位目录$interfaceDir出错!"; # 定位到插件目录
+    or die "Failed to locate to directory of $interfaceDir!";  
+
 
 my $IDFile= "ClsID_".$tmpPluginName.".h";
 my $newIDFile= "ClsID_".$pluginName.".h";
@@ -136,47 +140,47 @@ my $IxFile= "Ix_".$tmpPluginName.".h";
 my $newIxFile= "Ix_".$pluginName.".h";
 
 copy $IDFile, $newIDFile
-    or warn "从$IDFile拷贝到$newIDFile出错\n";
+    or warn "Failed to copy from $newIDFile to $IDFile\n";
 
 copy $IxFile, $newIxFile
-    or warn "从$IxFile拷贝到$newIxFile出错\n";
+    or warn "Failed to Copy from $newIxFile to $IxFile\n";
 
-# 替换接口文件内容(生成GUID)
+# Replace the contents fo interface files and generate GUID
 my @files = ($newIDFile, $newIxFile);
-foreach my $file  (@files)  { # 查找当前目录下所有文件
+foreach my $file  (@files)  { # Find all the files 
     print "$file\n";
 
     open(FILE, $file)
-        or warn "打开文件$file失败!\n";
+        or warn "Failed to open $file!\n";
     my @lines = <FILE>;
     close FILE;
 
     open(OUT, "> $file")
-        or warn "打开文件$file失败!\n";
+        or warn "Failed to open $file!\n";
 
     foreach my $line (@lines) {
-        # 替换插件名
+        # replace plugin name
         $line =~ s/$tmpPluginName/$pluginName/g;
 
-        # 替换GUID
+        # replace GUID
         $line =~ s/(CLSID_.*\(\")(.*)(\"\);)/$1$guid$3/g;
-        print OUT $line; # 写回到原文件
+        print OUT $line; # write back to the original file
     }
     close OUT;
 }
 
-# STEP4 替换版本信息
+# STEP4 replace version information
 ######################################################
 
-# 先定位到原始目录
+# Firstly,locate to plugin 
 chdir($curdir."\/".$pluginName);
 
 my $rcfile = $pluginName . ".rc";
 
 open(FILE, $rcfile)
-    or die "打开文件$rcfile失败!\n";
+    or die "Failed to open $rcfile!\n";
 
-# 读取模板文件  
+# Read template file  
 my $content = do { local $/; <FILE>; };
 close FILE;
 
@@ -187,8 +191,8 @@ $content =~ s/(VALUE\s*\"ProductName\",\s*)\".*?\"/$1 \"$ProductName\"/g;
 
 
 open(OUT, "> $rcfile")
-    or die "打开文件$rcfile失败!\n";
-print OUT $content; # 写回到原文件
+    or die "Failed to open $rcfile!\n";
+print OUT $content; # Write back
 close OUT;
 
 
