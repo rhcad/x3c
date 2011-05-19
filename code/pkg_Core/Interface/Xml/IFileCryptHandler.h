0f76a9d8e4c6cb2c1f9dd2f42f550ba0fed8e3ef
rhcad <rhcad@hotmail.com>
2011-05-19 04:17:09 +0000

    HRESULT to int.

--- code/pkg_Core/Interface/Xml/IFileCryptHandler.h
+++ code/pkg_Core/Interface/Xml/IFileCryptHandler.h
@@ -13,9 +13,9 @@ interface IFileCryptHandler
     /*!
         \param[in] filename 已加密文件的文件全名
         \param[out] buffer 输出文件内容，用 FreeBuffer 释放
-        \return S_OK 表示执行成功，其余表示失败
+        \return 0 表示执行成功，其余表示失败
     */
-    virtual HRESULT Decrypt(const wchar_t* filename, wchar_t** buffer) = 0;
+    virtual int Decrypt(const wchar_t* filename, wchar_t** buffer) = 0;
 
     //! 释放由 Decrypt 分配的内存资源
     virtual void FreeBuffer(wchar_t* buffer) = 0;
@@ -24,9 +24,9 @@ interface IFileCryptHandler
     /*! 
         \param srcfile 明文内容文件的文件全名
         \param destfile 加密文件的文件全名
-        \return S_OK 表示执行成功，其余表示失败
+        \return 0 表示执行成功，其余表示失败
     */
-    virtual HRESULT CryptFile(const wchar_t* srcfile, const wchar_t* destfile) = 0;
+    virtual int CryptFile(const wchar_t* srcfile, const wchar_t* destfile) = 0;
 };
 
 #endif // X3_IFILECRYPTHANDLER_H_
