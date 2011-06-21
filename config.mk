962b26a76016ce126f69f3fe5b3a007afeb37541
rhcad <rhcad@hotmail.com>
2011-06-21 02:47:17 +0000

    Add '-fPIC' to C_FLAGS_SO in config.mk

--- config.mk
+++ config.mk
@@ -6,7 +6,8 @@ INSTALL_DIR   =$(ROOTDIR)/code/bin
 PLUGINS_DIR   =$(INSTALL_DIR)/Plugins
 UNITTESTS_DIR =$(INSTALL_DIR)/UnitTests
 
-CC = g++
-CFLAGS  += -g
-C_FLAGS += -Wall -g
-C_FLAGS_SO = $(C_FLAGS) -shared -fpic
+CC          = g++
+CFLAGS     += -g
+C_FLAGS    += -Wall -g
+CFLAGS_SO   = $(CFLAGS) -fPIC
+C_FLAGS_SO  = $(C_FLAGS) -shared -fPIC
