de12a50c7d6e6661f4ccfbbe57e9ed3dff145093
rhcad <rhcad@hotmail.com>
2011-07-05 09:36:52 +0000

    Add notes.

--- code/pkg_Core/Interface/Module/XModuleImpl.h
+++ code/pkg_Core/Interface/Module/XModuleImpl.h
@@ -32,7 +32,8 @@ static Cx_Module    s_x3Module;
     \param hModule DLL handle of this plugin, skip if NULL.
     \return the module object in this plugin.
     \see x3InternalCreateObject, x3GetClassEntryTable
-    \see x3InitializePlugin, x3UninitializePlugin
+    \see OUTAPI bool x3InitializePlugin()
+    \see OUTAPI void x3UninitializePlugin()
 */
 OUTAPI Ix_Module* x3GetModuleInterface(Ix_ObjectFactory* pFactory, HMODULE hModule)
 {
@@ -67,7 +68,8 @@ static long GetClassCount(BYTE minType)
     \param[in] nMaxCount max count of elements in pTable.
     \return element count of actual copied. Return actual count if pTable is NULL.
     \see x3GetModuleInterface
-    \see x3InitializePlugin, x3UninitializePlugin
+    \see OUTAPI bool x3InitializePlugin()
+    \see OUTAPI void x3UninitializePlugin()
 */
 OUTAPI DWORD x3GetClassEntryTable(DWORD* pBuildInfo, DWORD* pEntrySize,
                                   X3CLASSENTRY* pTable, DWORD nMaxCount)
