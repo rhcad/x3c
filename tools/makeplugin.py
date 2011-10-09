b4865f10e4e3a0455d1de61199947a63d447f82b
rhcad <rhcad@hotmail.com>
2011-10-09 23:26:42 +0000

    makeplugin.py: Add 'view' for the param of 'basetype'. Skip project files while copying files in pkg_Example.

--- tools/makeplugin.py
+++ tools/makeplugin.py
@@ -2,15 +2,20 @@
 """
 Function:   Create a plugin project based on Win32DllTempl or MFCExtTempl.
             This script has been tested with ActivePython 2.7.
+
 Usage:      makeplugin.py projname pkgname basetype
             projname: name of the new project.
             pkgname: package name of the new project, the default value
                 is 'pkg_Example'.
-            basetype: 'mfc' or 'win32', the default value is 'win32'.
+            basetype: 'mfc', 'win32', 'view' or project name in pkg_Example, 
+                the default value is 'win32'.
+
 Creator:    ooyg <rhcad@hotmail.com>
 Date:       2011.10.9
 ChangeList:
     1. Implemented the function: multireplace, copyfiles, makeproj
+    2. Add 'view' for the param of 'basetype'.
+       Skip project files while copying files in pkg_Example. [2011/10/10]
 """
 
 import os, sys, re
@@ -26,14 +31,15 @@ def copyfiles(srcdir, destdir, pairs, callback=None):
         return
     if not os.path.exists(destdir):
         os.makedirs(destdir)
+    
     for fn in os.listdir(srcdir):
         srcfile = os.path.join(srcdir, fn)
         destfile = os.path.join(destdir, multireplace(fn, pairs))
+        
         if os.path.isdir(srcfile):
             copyfiles(srcfile, destfile, pairs)
-        if os.path.isfile(srcfile) \
-           and not os.path.exists(destfile) \
-           and (not callback or callback(fn, pairs)):
+        if os.path.isfile(srcfile) and not os.path.exists(destfile) \
+                and (not callback or callback(fn, pairs)):
             open(destfile, "wb").write(open(srcfile, "rb").read())
             text = open(destfile).read()
             newtext = multireplace(text, pairs)
@@ -56,16 +62,20 @@ def makeproj(projname, pkgname, baseproj):
 
     destdir = os.path.join(codepath, pkgname, 'Modules', projname)
     pairs = {baseproj:projname, 'pkg_Example':pkgname}
-    copyfiles(basepath, destdir, pairs)
-
+    
     def matchfile(filename, pairs):
+        if filename.find(".dsp") > 0 or filename.find(".vc") > 0:
+            return False
+        return True
+    copyfiles(basepath, destdir, pairs, matchfile)
+
+    def matchproj(filename, pairs):
         for key in pairs.keys():
             if filename.startswith(key):
                 return True
         return False
-
     projects = os.path.abspath('../projects/msvc/vcproj')
-    copyfiles(projects, projects, pairs, matchfile)
+    copyfiles(projects, projects, pairs, matchproj)
 
 if __name__=="__main__":
     def inputparam(index, prompt, default=''):
@@ -73,16 +83,16 @@ if __name__=="__main__":
             ret = sys.argv[index]
         else:
             ret = raw_input(prompt)
-        if ret == '':
-            ret = default
+        if ret == '': ret = default
         return ret
     
     projname = inputparam(1, 'Project name: ')
-    pkgname = inputparam(2, 'Package name (default: pkg_Example): ', 'pkg_Example')
-    baseproj = inputparam(3, 'Template project (win32|mfc): ', 'win32')
-    if baseproj == 'win32':
-        baseproj = 'Win32DllTempl'
-    if baseproj == 'mfc':
-        baseproj = 'MFCExtTempl'
-
+    pkgname  = inputparam(2, 'Package name (default: pkg_Example): ', 'pkg_Example')
+    baseproj = inputparam(3, 'Template project (win32, mfc, view, '   \
+                             'or project name in pkg_Example): ', 'win32')
+                
+    if baseproj == 'win32': baseproj = 'Win32DllTempl'
+    if baseproj == 'mfc':   baseproj = 'MFCExtTempl'
+    if baseproj == 'view':  baseproj = 'HelloView'
+    
     makeproj(projname, pkgname, baseproj)
