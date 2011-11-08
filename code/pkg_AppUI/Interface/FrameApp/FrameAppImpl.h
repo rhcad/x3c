9512eafb38977be9ce65073b6cd7f61018ea1648
rhcad <rhcad@hotmail.com>
2011-11-08 07:17:38 +0000

    Fix MSC_VER

--- code/pkg_AppUI/Interface/FrameApp/FrameAppImpl.h
+++ code/pkg_AppUI/Interface/FrameApp/FrameAppImpl.h
@@ -110,7 +110,7 @@ HINSTANCE CFrameApp::LoadAppLangResourceDLL()
 }
 
 
-#if _MSC_VER >= 1200     // not VC6
+#if _MSC_VER > 1200     // not VC6
 #if defined _M_IX86
 #pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='x86' publicKeyToken='6595b64144ccf1df' language='*'\"")
 #elif defined _M_IA64
